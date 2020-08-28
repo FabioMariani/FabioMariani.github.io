69abf109a7a561b8f2138155d50d5b19fe1dabb7
FabioMariani <fabios.ap93@hotmail.com>
2020-08-28 18:23:43 +0200

    Week2

--- ModernMigrants/data.js
+++ ModernMigrants/data.js
@@ -52,6 +52,56 @@ var data = [
     },
 
     {
+        "title":"Week 2",
+        "period":["24-08-2020","28-08-2020"],
+        "highlights":
+                    [
+                        "Improvement of data scraping function",
+                        "Development of the function that splits the individual events of the provenance",
+                        "Planning of different approaches to knowledge extraction and first simple experiments"
+                    ],
+        "achievements":[
+            {
+                "title":"NGA data scraping",
+                "text":"By better analyzing the NGA data I realized that I had left out an important factor that excessively increased the number of works of art analyzed: the <b>category</b>. \
+                Excluding from the scraping process all the works that do not belong to the category \"Paintings\" drastically reduces the number of works and you get a significant improvement in performance."
+            },
+            {
+                "title":"Events Splitting",
+                "text":"The function that splits the events of a provenance is conceptually very simple: given a text the function must divide it for each \";\" and \".\" outside the brackets. \
+                The real big problem in doing this depends on the abbreviations accompanied by \".\" in the text. After several attempts, the best solution was to create an auxiliary algorithm that would identify \
+                all the abbreviations (words ending with \".\" whose length does not exceed 4 characters) and generate a list of them. However, since there are no strict rules to distinguish abbreviations from\
+                 short words (e.g. \"York.\" and \"Capt.\"), a small human intervention is needed to remove some words from the list (4 words in total).\
+                  Also take into account \"NGA.\" which is in fact an abbreviation, but we consider it to be a word."
+            },
+            {
+                "title":"Knowledge Extraction",
+                "text":"I have reached the crucial point of the pipeline. Having started working with the provenance data in these 2 weeks I got a better idea of how they are structured \
+                 (or rather, unstructured). For the moment I have thought of 2 action plans. The first plan, the one I would like to realize, involves the fully automatic (or at most semi-automatic)\
+                  extraction of information. For the moment I have minimized the problem to the extraction from an event of people, transaction method, place and date.\
+                <p>The second plan, if the first one is not feasible, is the manual annotation of the data. Obviously a longer process that requires careful planning.\
+                 I consider this the most extreme solution.</p>\
+                <p>This week I have already started to \"play\" with some NLP methods. In particular with the technique of Part of Speech Tagging (POS tagging) of which a test file is already available.\
+                 This technique tracks for each token (word) its grammatical role (e.g. noun, proper noun, verb, adjective...). \
+                 This technique, although it needs improvements, is not enough to extract useful information but it could be useful in combination with other techniques that I will deepen in the next days.</p>"
+            }
+        ],
+        "brainstorming":[
+            {
+                "title":"No particular issues this week",
+                "text":""
+            }
+        ],
+        "future":[
+            {
+                "title":"Named Entity Recognition",
+                "text":"Next week I will focus on improving POS tagging and introducing new techniques, in particular Named Entity Recongition (NER).\
+                 This technique identifies within a structured text entities with proper names (e.g. people, organizations or places) but can also be used to extract temporal information. "
+            }
+        ]
+    },
+
+    {
         "title":"",
         "period":["",""],
         "highlights":
